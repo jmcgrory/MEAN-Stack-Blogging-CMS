694120022093b2c6463584c92afc23c2a624cfc3
Jamie McGrory <contact@jmcgr.co.uk>
2018-04-30 21:58:43 +0100

    PostalService Formatting

--- angular-src/src/app/services/postal.service.ts
+++ angular-src/src/app/services/postal.service.ts
@@ -7,109 +7,140 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class PostalService {
 
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService
-  ){}
+    constructor(
 
-  // Get variable amount of posts...
-  getLimitPosts(limit){
-    let headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.get<Post[]>(
-      'http://localhost:3000/posts/limit',
-      { 
-        headers: headers,
-        params: new HttpParams().set('limit', limit)
-      }
-    );
-  }
+        private http: HttpClient,
 
-  // Get variable amount of posts...
-  getPostByURL(url:string){
-    let headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.get<Post>(
-      'http://localhost:3000/posts/post',
-      {
-        headers: headers,
-        params: new HttpParams().set('url', url)
-      }
-    );
-  }
-  
-  // Get variable amount of posts...
-  getPostByID(id){
-    let headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.get<Post>(
-      'http://localhost:3000/posts/post',
-      {
-        headers: headers,
-        params: new HttpParams().set('id', id)
-      }
-    );
-  }
+        private authService: AuthService
+        
+    ){}
 
-  // Get all posts
-  getAllPosts(){
-    let headers = new HttpHeaders({'Content-Type': 'application/json'});
-    return this.http.get<Post[]>(
-      'http://localhost:3000/posts/all',
-      { headers: headers }
-    );
-  }
+    // Get variable amount of posts...
+    getLimitPosts(limit){
 
-  // Delete One Post
-  deletePost(id){
-    let token = this.authService.getToken();
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': token
-    });
-    return this.http.post(
-      'http://localhost:3000/posts/delete',
-      { _id: id },
-      { headers: headers }
-    );
-  }
-  
-  // Add Generic Post
-  addPost(){
-    let token = this.authService.getToken();
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': token
-    });
-    return this.http.post(
-      'http://localhost:3000/posts/add', {},
-      { headers: headers }
-    );
-  }
-  
-  // ?Active Post
-  postActive(id, active){
-    let token = this.authService.getToken();
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': token
-    });
-    return this.http.post(
-      'http://localhost:3000/posts/active',
-      { id: id, active: active },
-      { headers: headers }
-    );
-  }
-  
-  // ?Active Post
-  updatePost(post){
-    let token = this.authService.getToken();
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': token
-    });
-    return this.http.post(
-      'http://localhost:3000/posts/update',
-      { post: post },
-      { headers: headers }
-    );
-  }
+        let headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+        return this.http.get<Post[]>(
+            'http://localhost:3000/posts/limit',
+            { 
+                headers: headers,
+                params: new HttpParams().set('limit', limit)
+            }
+        );
+
+    }
+
+    // Get variable amount of posts...
+    getPostByURL(url:string){
+
+        let headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+        return this.http.get<Post>(
+            'http://localhost:3000/posts/post',
+            {
+                headers: headers,
+                params: new HttpParams().set('url', url)
+            }
+        );
+
+    }
+
+    // Get variable amount of posts...
+    getPostByID(id){
+
+        let headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+        return this.http.get<Post>(
+            'http://localhost:3000/posts/post',
+            {
+                headers: headers,
+                params: new HttpParams().set('id', id)
+            }
+        );
+
+    }
+
+    // Get all posts
+    getAllPosts(){
+
+        let headers = new HttpHeaders({'Content-Type': 'application/json'});
+        
+        return this.http.get<Post[]>(
+            'http://localhost:3000/posts/all',
+            { headers: headers }
+        );
+
+    }
+
+    // Delete One Post
+    deletePost(id){
+
+        let token = this.authService.getToken();
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': token
+        });
+
+        return this.http.post(
+            'http://localhost:3000/posts/delete',
+            { _id: id },
+            { headers: headers }
+        );
+
+    }
+
+    // Add Generic Post
+    addPost(){
+
+        let token = this.authService.getToken();
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': token
+        });
+
+        return this.http.post(
+            'http://localhost:3000/posts/add', {},
+            { headers: headers }
+        );
+
+    }
+
+    // ?Active Post
+    postActive(id, active){
+
+        let token = this.authService.getToken();
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': token
+        });
+
+        return this.http.post(
+            'http://localhost:3000/posts/active',
+            { id: id, active: active },
+            { headers: headers }
+        );
+
+    }
+
+    // ?Active Post
+    updatePost(post){
+
+        let token = this.authService.getToken();
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': token
+        });
+
+        return this.http.post(
+            'http://localhost:3000/posts/update',
+            { post: post },
+            { headers: headers }
+        );
+
+    }
 
 }
