0a8549ae8773b3b7dbabd5eb2e211e512ac61dc9
Jamie McGrory <contact@jmcgr.co.uk>
2018-05-28 21:32:29 +0100

    Including DB File Temporarily

---
+++ config/database.js
@@ -0,0 +1,11 @@
+//************************//
+//**** MongoDB Config ****//
+//************************//
+
+module.exports = {
+
+    database: "mongodb://user_jamie_mcgrory:V5NOP1aUlibpsCTmXr1B@ds233895.mlab.com:33895/jmcgrory",
+
+    secret: "sL&2sec@IHpLLZu9xH*21Nu^i%im",
+
+}
